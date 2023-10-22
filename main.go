16dcb123653ec6750a710cba9d10c3cf7eb9db29
aaron-jencks <aaronjencks@aol.com>
2023-10-21 22:06:24 -0500

    moved constants to flags for executable

--- main.go
+++ main.go
@@ -20,8 +20,22 @@ func main() {
 	p := message.NewPrinter(language.English)
 
 	var checkpoint_path string = "./checkpoint.bin"
-	flag.StringVar(&checkpoint_path, "check", checkpoint_path, "indicates where to save checkpoints")
+	var procs uint = uint(runtime.NumCPU()) << 1
+	var ubsize uint = 8
+	var display_poll time.Duration = 100 * time.Millisecond
+
+	flag.StringVar(&checkpoint_path, "check", checkpoint_path, "indicates where to save checkpoints, defaults to ./checkpoint.bin")
+	flag.UintVar(&procs, "procs", procs, "specifies how many threads to use for processing, defaults to 2*cpu cores")
+	flag.UintVar(&ubsize, "size", ubsize, "specifies the size of the board to run the program on, defaults to 8")
+	flag.DurationVar(&display_poll, "display", display_poll, "specifies how often to update the statistics in the terminal, defaults to 100 ms")
 	flag.Parse()
+
+	if ubsize > 255 {
+		p.Printf("board size must be small enough to fit within one byte (<= 255), received: %d\n", ubsize)
+		return
+	}
+
+	bsize := uint8(ubsize)
 
 	var counter, explored, repeated uint64 = 0, 0, 0
 	var clock, elock, rlock sync.RWMutex = sync.RWMutex{}, sync.RWMutex{}, sync.RWMutex{}
@@ -31,9 +45,9 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	initialBoards := walking.FindInitialBoards(uint(runtime.NumCPU()), 8)
+	initialBoards := walking.FindInitialBoards(procs, bsize)
 
-	p.Printf("Created %d boards for %d processors\n", len(initialBoards), runtime.NumCPU())
+	p.Printf("Created %d boards for %d processors\n", len(initialBoards), procs)
 
 	save_chan := make(chan bool)
 
@@ -69,13 +83,13 @@ func main() {
 			clock.RLock()
 			elock.RLock()
 			rlock.RLock()
-			erate := uint64(float64(explored-prev_explored) / 0.1)
+			erate := uint64(float64(explored-prev_explored) / display_poll.Seconds())
 			p.Printf("\r%d found %d explored %d repeated @ %d boards/sec", counter, explored, repeated, erate)
 			prev_explored = explored
 			clock.RUnlock()
 			elock.RUnlock()
 			rlock.RUnlock()
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(display_poll)
 		}
 	}
 }
