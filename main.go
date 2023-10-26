d0ed2e553207fdc50ee51b04c89befa7339853ee
Aaron Jencks <aaron.jencks@syncbak.com>
2023-10-26 11:58:11 -0500

    added back in cpu profiling ability

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"runtime/pprof"
 	"sync"
 	"syscall"
 	"time"
@@ -27,6 +28,7 @@ func main() {
 	var display_poll time.Duration = 2 * time.Second
 	var save_interval time.Duration = 5 * time.Minute
 	var cache_update_interval time.Duration = time.Second
+	var cpu_profile_file string = ""
 
 	flag.StringVar(&checkpoint_path, "check", checkpoint_path, "indicates where to save checkpoints, defaults to ./checkpoint.bin")
 	flag.UintVar(&procs, "procs", procs, "specifies how many threads to use for processing, defaults to the number of cpu cores")
@@ -34,11 +36,22 @@ func main() {
 	flag.DurationVar(&display_poll, "display", display_poll, "specifies how often to update the statistics in the terminal, defaults to 2s")
 	flag.DurationVar(&save_interval, "save", save_interval, "specifies how often to save the state of the walker, defaults to 5m")
 	flag.DurationVar(&cache_update_interval, "walkupdate", cache_update_interval, "specifies how often the walkers should take their cached data and sync it with the cache, defaults to 1s")
+	flag.StringVar(&cpu_profile_file, "cpuprofile", cpu_profile_file, "specifies where to save pprof data to if supplied, leave empty to disable")
 	flag.Parse()
 
 	if ubsize > 255 {
 		p.Printf("board size must be small enough to fit within one byte (<= 255), received: %d\n", ubsize)
 		return
+	}
+
+	if cpu_profile_file != "" {
+		f, err := os.Create(cpu_profile_file)
+        if err != nil {
+            p.Printf("failed to start cpu profiling: %s\n", err.Error())
+			return
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
 	}
 
 	bsize := uint8(ubsize)
