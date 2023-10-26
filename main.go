64a8c85fad306dbd0f7a1627eb7834111faaa0c0
Aaron Jencks <aaron.jencks@syncbak.com>
2023-10-26 12:04:53 -0500

    added memory profiling

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 	var save_interval time.Duration = 5 * time.Minute
 	var cache_update_interval time.Duration = time.Second
 	var cpu_profile_file string = ""
+	var mem_profile_file string = ""
 
 	flag.StringVar(&checkpoint_path, "check", checkpoint_path, "indicates where to save checkpoints, defaults to ./checkpoint.bin")
 	flag.UintVar(&procs, "procs", procs, "specifies how many threads to use for processing, defaults to the number of cpu cores")
@@ -37,6 +38,7 @@ func main() {
 	flag.DurationVar(&save_interval, "save", save_interval, "specifies how often to save the state of the walker, defaults to 5m")
 	flag.DurationVar(&cache_update_interval, "walkupdate", cache_update_interval, "specifies how often the walkers should take their cached data and sync it with the cache, defaults to 1s")
 	flag.StringVar(&cpu_profile_file, "cpuprofile", cpu_profile_file, "specifies where to save pprof data to if supplied, leave empty to disable")
+	flag.StringVar(&mem_profile_file, "memprofile", mem_profile_file, "specifies where to save the pprof memory data to if supplied, leave empty to disable")
 	flag.Parse()
 
 	if ubsize > 255 {
@@ -44,14 +46,43 @@ func main() {
 		return
 	}
 
+	if cpu_profile_file != "" && mem_profile_file == cpu_profile_file {
+		p.Printf("cpu profile and memory profile cannot be written to the same file!\n")
+		return
+	}
+
 	if cpu_profile_file != "" {
 		f, err := os.Create(cpu_profile_file)
-        if err != nil {
-            p.Printf("failed to start cpu profiling: %s\n", err.Error())
+		if err != nil {
+			p.Printf("failed to start cpu profiling: %s\n", err.Error())
 			return
-        }
-        pprof.StartCPUProfile(f)
-        defer pprof.StopCPUProfile()
+		}
+		pprof.StartCPUProfile(f)
+		defer func() {
+			if mem_profile_file != "" {
+				f, err := os.Create(mem_profile_file)
+				if err != nil {
+					p.Printf("failed to start memory profiling: %s\n", err.Error())
+					return
+				}
+				pprof.WriteHeapProfile(f)
+				err = f.Close()
+				if err != nil {
+					p.Printf("failed to close memory profile file: %s\n", err.Error())
+				}
+			}
+			pprof.StopCPUProfile()
+		}()
+	}
+
+	if mem_profile_file != "" {
+		f, err := os.Create(mem_profile_file)
+		if err != nil {
+			p.Printf("failed to start memory profiling: %s\n", err.Error())
+			return
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
 	}
 
 	bsize := uint8(ubsize)
