44b2e4ec079f0c01f79272e75d683387135ae9a8
aaron-jencks <aaronjencks@aol.com>
2023-10-28 11:10:50 -0500

    added utility for examining checkpoints

---
+++ cmd/checkpoint-stats/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/aaron-jencks/reversi/checkpoints"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+func Main() {
+	p := message.NewPrinter(language.English)
+	restore_file := flag.String("file", "../checkpoint.bin", "specifies the file to parse, defaults to ../checkpoint.bin")
+	flag.Parse()
+	stats, err := checkpoints.CheckpointStats(*restore_file)
+	if err != nil {
+		p.Printf("Failed to read %s: %s\n", restore_file, err.Error())
+		return
+	}
+	p.Println(stats)
+}
